1256747a2e455a8820cc139d8aaf854df087378f
backy3000 <32287102+backy3000@users.noreply.github.com>
2017-09-30 23:20:07 +0700

    1

--- Day2/Reco001_Digits.m
+++ Day2/Reco001_Digits.m
@@ -1,9 +1,9 @@
 function Reco001_Digits()
     fprintf('\nLoad du lieu train');
-    imgTrainAll=loadMNISTImages('train-images.idx3-ubyte');    
-    lblTrainAll = loadMNISTLabels('train-labels.idx1-ubyte');
+    imgTrainAll=loadTImages('train-images.idx3-ubyte');    
+    lblTrainAll = loadLabels('train-labels.idx1-ubyte');
     fprintf('\nLoad du lieu test');
-    imgTestAll=loadMNISTImages('t10k-images.idx3-ubyte');    
-    lblTestAll = loadMNISTLabels('t10k-labels.idx1-ubyte');
+    imgTestAll=loadImages('t10k-images.idx3-ubyte');    
+    lblTestAll = loadLabels('t10k-labels.idx1-ubyte');
     fprintf('\nKet thuc.');
 end
